2dd3468a830f957a5d1270118bc54c7759df0361
Otavio Rodrigues <otavio.rodri3@gmail.com>
2023-09-05 09:59:39 -0300

    Adição dos ícones a pagina Contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,25 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+import styles from '../layouts/Footer.module.css'
+
 function Contact() {
     return (
         <div>
             <h1>Contact</h1>
+            <div className={styles.contact}>
+                <ul className={styles.social_list_contact}>
+                    <li>
+                        <FaFacebook />
+                    </li>
+                    <li>
+                        <FaInstagram />
+                    </li>
+                    <li>
+                        <FaLinkedin />
+                    </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
